bbeba0c4558e46370a9964a02b67270601ab273a
Dennis Snijder <dennissnijder97@gmail.com>
2016-12-06 14:41:48 +0100

    init commit

---
+++ src/ROCMN/ROCMailer/ROCMailer.php
@@ -0,0 +1,14 @@
+<?php
+namespace ROCMN\ROCMailer;
+
+/**
+ * Class ROCMailer
+ * @package ROCMN\ROCMailer
+ * @author Dennis Snijder <Dennis@Snijder.io>
+ */
+class ROCMailer
+{
+    public function __construct()
+    {
+    }
+}
